8a3d4679dc4843faf401e71c75b0d1cffbb5ee18
Christian Zamora <christianzamoraburgos@gmail.com>
2021-03-17 21:40:32 -0600

    added notices

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -9,11 +9,13 @@ class ContactsController < ApplicationController
     @contact.request = request
     if @contact.deliver
       # flash.now[:notice] = 'Thank you for your message. I will contact pretty you soon!'
-      redirect_to new_contact_path, alert: "BRILLIANT!, " + "" + " I will contact you pretty soon, thank you."
+      # redirect_to new_contact_path, alert: "BRILLIANT!, " + "" + " I will contact you pretty soon, thank you."
       # render json: {message: "Email sent successfully"}
+      flash.now[:error] = nil
+      redirect_to root_path, notice: 'Gracias, tu mensaje se ha enviado'
     else
-      # flash.now[:error] = 'Oops! something is wrong, can you please send me an email instead?'
-      # render :new
+      flash.now[:error] = 'Hay un problema, nos puedes mandar un mail o un mensaje porfavor?'
+      render :new
       #redirect_to new_contact_path, alert: 'Oops! something is wrong, can you please send me an email instead?, please'
     end
   end
