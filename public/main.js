628709a91a9c0d2131347c631fb001d656c2eb24
Christian Zamora <christianzamoraburgos@gmail.com>
2021-05-05 13:14:05 -0500

    changed navbar link Home for Inicio

--- public/main.js
+++ public/main.js
@@ -2,13 +2,13 @@ $(function() {
     
     "use strict";
     
+
     //===== Prealoder
     
     $(window).on('load', function(event) {
         $('.preloader').delay(500).fadeOut(500);
     });
-    
-    
+     
     //===== Sticky
     
     $(window).on('scroll', function(event) {    
